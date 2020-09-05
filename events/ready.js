0b12f04a2d827c93da3d864adb9e3b320a4ebb1a
Agent-Mimos <mihaigifei0@gmail.com>
2020-09-05 20:45:31 +0300

    .

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({
+    disableEveryone: true
+});
+
+module.exports = async (bot, message) => {
+    console.log(`${bot.user.tag} is up and running. | ${bot.guilds.cache.size.toLocaleString()} servers | ${bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users`);
+    bot.user.setPresence({
+            activity: {
+                name: `The Developer Agent Mimos#0001 ✨`,
+                type: 'WATCHING',
+            },
+            status: 'dnd'
+        })
+        .catch(console.error);
+};
